01466a905a7eef06b65e649836520a53c0ca2b70
yuHuang01 <huang.y0112@gmail.com>
2021-01-26 11:03:57 +0100

    Clean up for Output Management

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import _ from 'lodash';
-import './webpack.css';
-import WebpackImg from './wpimg.png';
-import Data from './data.xml';
-import Note from './data.csv';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  const myImg = new Image();
-  myImg.src = WebpackImg;
-
-  element.appendChild(myImg);
-
-  console.log(Data);
-  console.log(Note)
 
   return element;
 };
