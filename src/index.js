5244b0faadefcb49725781a89f28dcdd8ee25efc
yuHuang01 <huang.y0112@gmail.com>
2021-01-26 10:03:56 +0100

    Loading images done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import _ from 'lodash'
-
+import _ from 'lodash';
+import './webpack.css';
+import WebpackImg from './wpimg.png';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
+  const myImg = new Image();
+  myImg.src = WebpackImg;
+
+  element.appendChild(myImg);
+  
   return element;
 };
 
