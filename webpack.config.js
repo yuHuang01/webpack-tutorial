45593fbae915ebecca4ffc49d4e26bfc27a99343
yuHuang01 <huang.y0112@gmail.com>
2021-01-26 09:35:38 +0100

    Asset Management

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,22 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+
+  /*Getting started
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },*/
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
 }
